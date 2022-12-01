8018a22340d2ddb9ccdc30a4acaab37b70eb24a9
dikshamtesgithub <119382373+dikshamtesgithub@users.noreply.github.com>
2022-12-01 00:14:56 -0800

    dash cchange
    hi

--- hello/src/hello/Hello.java
+++ hello/src/hello/Hello.java
@@ -2,7 +2,7 @@ package hello;
 
 public class Hello {
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		// TODO Auto-generated method stub
         
 		System.out.println("Welcome to git");
